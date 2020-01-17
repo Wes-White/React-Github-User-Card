b5ae12531c8376bbf5f69edb5d790a49ab15c187
Wes-White <wesleyrwhite86@gmail.com>
2020-01-17 07:10:57 -1000

    Fetching data from github

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      user: " "
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://api.github.com/users/Wes-White")
+      .then(res => res.json())
+      .then(user => {
+        console.log(user);
+      })
+      .catch(err =>
+        console.log("Oops we are having trouble locating the user", err)
+      );
+  }
+
+  render() {
+    return <div className="App"></div>;
+  }
 }
 
 export default App;
---
+++ user-card/src/components/UserCard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserCard = props => {
+  return <div></div>;
+};
+
+export default UserCard;
