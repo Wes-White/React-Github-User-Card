be4819fab3153466f182356fdfe1276951abe75d
Wes-White <wesleyrwhite86@gmail.com>
2020-01-18 06:59:43 -1000

    fetching user and followers with CDM

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -1,27 +1,54 @@
 import React from "react";
 import "./App.css";
+import UserCard from "./components/UserCard";
+import FollowerCard from "./components/FollowerCard";
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      user: " "
+      user: [],
+      followers: []
     };
   }
 
   componentDidMount() {
-    fetch("https://api.github.com/users/Wes-White")
+    Promise.all([
+      fetch("https://api.github.com/users/Wes-White"),
+      fetch("https://api.github.com/users/Wes-White/followers")
+    ])
+      .then(([res1, res2]) => {
+        return Promise.all([res1.json(), res2.json()]);
+      })
+      .then(([res1, res2]) => {
+        console.log("This is my first fetch", res1);
+        console.log("This is my second fetch", res2);
+      });
+  }
+  /*fetch("https://api.github.com/users/Wes-White")
       .then(res => res.json())
       .then(user => {
-        console.log(user);
+        console.log("Here is my user", user);
+        this.setState({ ...this.state, user: user });
       })
+
       .catch(err =>
         console.log("Oops we are having trouble locating the user", err)
-      );
-  }
+      )
+      .finally(fetch("https://api.github.com/users/Wes-White/followers"))
+      .then(res => res.json())
+      .then(followers => {
+        console.log(followers);
+      })
+      .catch(err => err);
+  } */
 
   render() {
-    return <div className="App"></div>;
+    return (
+      <div className="App">
+        <UserCard data={this.state.user} />
+      </div>
+    );
   }
 }
 
--- user-card/src/components/UserCard.js
+++ user-card/src/components/UserCard.js
@@ -1,7 +1,31 @@
 import React from "react";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardLink,
+  CardTitle,
+  CardSubtitle
+} from "reactstrap";
 
 const UserCard = props => {
-  return <div></div>;
+  return (
+    <div>
+      <Card className="user-card">
+        <CardBody>
+          <CardTitle>{props.data.name}</CardTitle>
+          <CardSubtitle>{props.data.bio}</CardSubtitle>
+        </CardBody>
+        <img width="100%" src={props.data.avatar_url} alt="GH-Image" />
+        <CardBody>
+          <CardText>{props.data.bio}</CardText>
+          <CardText>Followers: {props.data.followers}</CardText>
+          <CardText>Following: {props.data.following}</CardText>
+        </CardBody>
+      </Card>
+    </div>
+  );
 };
 
 export default UserCard;
